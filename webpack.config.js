9d6beaf3e8469019b237c0f5488e385c2297c35e
Roy Tang <roytang.rb@gmail.com>
2019-08-23 17:22:09 +0800

    initial commit: env setup

---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const cleanWebpackPlugin = require('clean-webpack-plugin')
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: __dirname + "/src/index.js",
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+        publicPath: '' //relative path
+    },
+    resolve: {
+        modules: ['node_modules'],
+        alias: {
+            'TweenLite': 'gsap/src/minified/TweenLite.min.js',
+            'TweenMax': 'gsap/src/minified/TweenMax.min.js',
+            'TimelineLite': 'gsap/src/minified/TimelineLite.min.js',
+            'TimelineMax': 'gsap/src/minified/TimelineMax.min.js',
+            'ScrollMagic': 'scrollmagic/scrollmagic/minified/ScrollMagic.min.js',
+            'animation.gsap': 'scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js',
+            'debug.addIndicators': 'scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js',
+            // 'snapsvg': 'snapsvg/dist/snap.svg.js',
+        }
+    },
+    module: {
+        rules: [
+            // {
+            //     test: require.resolve('snapsvg/dist/snap.svg.js'),
+            //     use: 'imports-loader?this=>window,fix=>module.exports=0',
+            // },
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: [
+                    /node_modules/
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },{
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                use:['url-loader']
+            },{
+                test: /\.(gif|jpe?g|svg)$/i,
+                use: [
+                  'file-loader',
+                  {
+                    loader: 'image-webpack-loader',
+                    options: {
+                      //bypassOnDebug: true, // webpack@1.x
+                      //disable: true, // webpack@2.x and newer
+                      mozjpeg: {
+                        progressive: true,
+                        quality: 65
+                      },
+                    },
+                  },
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new cleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: __dirname + "/src/public/index.html",
+            inject: 'body'
+        }),
+        new ScriptExtHtmlWebpackPlugin({
+            //sync: /index/,
+            defaultAttribute: 'defer'
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+        }),
+        new CopyWebpackPlugin([
+            // './favicon.ico',
+            './README.md',
+            // './favicon.png',
+        ], {}) // options
+    ],
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        },
+        usedExports: true
+    },
+    devServer: {
+        contentBase: './src/public',
+        port: 7700,
+    }
+};
