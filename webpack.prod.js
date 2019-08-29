7d91fda813503d1b983e97f2d57c0fddf084b311
Roy Tang <roytang.rb@gmail.com>
2019-08-29 23:09:53 +0800

    add gzip prod plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,12 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CompressionPlugin = require('compression-webpack-plugin')
 
 module.exports = merge(common, {
     mode: 'production',
+    plugins: [
+    	new CompressionPlugin({
+    		test: /\.js(\?.*)?$/i, //default gzip algo, zip js files
+    	})
+    ],
 });
