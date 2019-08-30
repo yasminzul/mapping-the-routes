087e51c7eaf9dce7de82a55e1f9d7a4e2d3ea025
Roy Tang <roytang.rb@gmail.com>
2019-08-30 12:25:58 +0800

    create groups for china path and world path

--- src/modules/d3-map.js
+++ src/modules/d3-map.js
@@ -1,5 +1,5 @@
 import * as d3 from 'd3'
-import * as topojson from 'topojson'
+// import * as topojson from 'topojson'
 
 const D3MAP = {}
 const $ = q => document.querySelector(q)
@@ -10,8 +10,6 @@ D3MAP.renderMap = function(){
 	    .domain([10000,100000,500000,1000000,5000000,10000000,50000000,100000000,500000000,1500000000])
 	    .range(["rgb(247,251,255)", "rgb(222,235,247)", "rgb(198,219,239)", "rgb(158,202,225)", "rgb(107,174,214)", "rgb(66,146,198)","rgb(33,113,181)","rgb(8,81,156)","rgb(8,48,107)","rgb(3,19,43)"]);
 
-	var path = d3.geoPath();
-
 	var svg = d3.select("#geo-map-container")
 	            .append("svg")
 	            .attr('x', 0)
@@ -19,13 +17,11 @@ D3MAP.renderMap = function(){
 	            .attr('viewBox', '0 0 960 500')
 	            .attr('id', 'geo-map');
 
-	var g = svg.append("g")
-
 	var width = $('#geo-map').getBBox().width
 	var height = $('#geo-map').getBBox().height
 	var projection = d3.geoMercator()
 	                   .scale(130)
-	                   // .translate([width / 2, height / 3]);
+	                   // .translate([width / 2, height / 1.5]);
 
 	var path = d3.geoPath().projection(projection);
 
@@ -40,19 +36,16 @@ D3MAP.renderMap = function(){
 	})
 
 	function ready(values) {
-		var [data, china, population] = values
+		var [world, china, population] = values
 	  var populationById = {};
 
 	  population.forEach(function(d) { populationById[d.id] = +d.population; });
-	  data.features.forEach(function(d) { d.population = populationById[d.id] });
+	  world.features.forEach(function(d) { d.population = populationById[d.id] });
 
-	  //add china features into world map
-	  data.features = data.features.concat(china.features)
-
-	  g
+	var g_world = svg.append("g")
 	    .attr("class", "countries")
 	    .selectAll("path")
-	    .data(data.features)
+	    .data(world.features)
 	    .enter().append("path")
 	      .attr("d", path)
 	      .style("fill", function(d) { return color(populationById[d.id]); })
@@ -75,10 +68,16 @@ D3MAP.renderMap = function(){
 	            .style("stroke-width",0.3);
 	        });
 
-	  svg.append("path")
-	      .datum(topojson.mesh(data.features, function(a, b) { return a.id !== b.id; }))
-	      .attr("class", "names")
-	      .attr("d", path);
+		var g_china = svg.append('g')
+	    .attr("class", "china-provinces")
+	    .selectAll("path")
+	    .data(china.features)
+	    .enter().append("path")
+	      .attr("d", path)
+	      .style("fill", 'none')
+	      .style('stroke', 'white')
+	      .style('stroke-width', 0.3)
+	      .style("opacity",0.8)
 	}
 }
 
