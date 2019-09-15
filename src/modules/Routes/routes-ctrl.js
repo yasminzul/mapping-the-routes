8e79842226cc5dce841806bbf05a2d5bc2fb167a
Roy Tang <roytang.rb@gmail.com>
2019-09-16 02:37:43 +0800

    fix textbox disapprear tween

--- src/modules/Routes/routes-ctrl.js
+++ src/modules/Routes/routes-ctrl.js
@@ -54,7 +54,7 @@ Malaysia.SceneCtrl = () => {
 		//map 1 part 1
 		.add(TweenMax.to($('#arrows'), 0.3, {opacity: 1, ease:Linear.easeNone}), 0)
 		.add(TweenMax.to($('#melaka-label'), 0.2, {opacity: 1, ease:Linear.easeNone}), 0.2)
-		.add(TweenMax.to($('#caption01'), 0.3, {opacity: 1, ease:Linear.easeNone}), 0.2)
+		.add(TweenMax.to($('#caption01'), 0.2, {opacity: 1, ease:Linear.easeNone}), 0.2)
 
 
 		.add(TweenMax.to($('#arrows'), 0.2, {opacity: 0, ease:Linear.easeNone}), 0.3)
@@ -65,7 +65,7 @@ Malaysia.SceneCtrl = () => {
 		.add(TweenMax.to($('#forest-fill'), 0.5, {opacity: 1, ease:Linear.easeNone}), 0.5)
 		.add(TweenMax.to($('#forest-label'), 0.3, {opacity: 1, ease:Linear.easeNone}), 0.7)
 
-  new ScrollMagic.Scene({ triggerElement: Malaysia.maps[0], triggerHook:'onLeave', duration: 700})
+  new ScrollMagic.Scene({ triggerElement: Malaysia.maps[0], triggerHook:'onLeave', duration: 700, reverse: true})
     .setTween(tween)
     .on('enter', function(){
     	this.setPin(this.triggerElement())
